f6f063733bf996591c2e66623978e3499c2d9d9d
Michael Mayer <michael@liquidbytes.net>
2012-03-07 20:42:38 +0100

    Added Bayeux support for Liquid Ajax

--- Liquid/Ajax/Handler/Bayeux.php
+++ Liquid/Ajax/Handler/Bayeux.php
@@ -8,46 +8,48 @@
  *
  * @category   Liquid
  * @package    Liquid_Ajax
- * @copyright  Copyright (c) 2010 Liquid Bytes Technologies (http://www.liquidbytes.net/)
+ * @copyright  Copyright (c) 2012 Liquid Bytes Technologies (http://www.liquidbytes.net/)
  * @license    http://www.liquidbytes.net/bsd.html New BSD License
  */
  
 require_once 'Liquid/Ajax/Handler/Abstract.php';
 
-class Liquid_Ajax_Handler_Stomp extends Liquid_Ajax_Handler_Abstract {   
+class Liquid_Ajax_Handler_Bayeux extends Liquid_Ajax_Handler_Abstract {   
     // PRIVATE VARIABLES
-    private $stomp;
-    private $address;
-    private $port;
+    private $bayeux;
+    private $url;
     private $user;
     private $password;    
     
     // CONSTRUCTOR
-    function __construct($address, $port, $user = '', $password = '')
+    function __construct($url, $user = '', $password = '')
     {   
-        $this->address      = $address;
-        $this->port         = $port;
+        $this->url          = $url;
         $this->user         = $user;
         $this->password     = $password;
     }    
     
     // PUBLIC FUNCTIONS
+    public function convertChannelName ($channel) {
+        return '/' . strtr($channel, '.', '/');        
+    }
+    
     public function connect()
     {        
-        $this->stomp = new Stomp('tcp://'.$this->address.':'.$this->port);
+        $this->bayeux = new Liquid_Ajax_Adapter_Bayeux($this->url);
         $this->connected = true;
     }
     
     public function disconnect()
     {
-        unset($this->stomp);
+        unset($this->bayeux);
         $this->connected = false;
     }        
     
     public function send($channel, $body, $retry = true)
     {        
         try { 
-            $this->stomp->send($channel, Zend_Json::encode($body));
+            $this->bayeux->publish($this->convertChannelName($channel), $body);
         } catch (Exception $e) {
             if($retry) {
                 return $this->send($channel, $body, false);
@@ -61,11 +63,11 @@ class Liquid_Ajax_Handler_Stomp extends Liquid_Ajax_Handler_Abstract {
     }
     
     public function sendHttpHeader () {
-        ignore_user_abort(true);
+        /* ignore_user_abort(true);
         header("Connection: close");
         header("Content-Encoding: none");
         header("Content-Length: 0");
-        flush();
+        flush(); */
     }      
 
     public function sendAggregatedResponse ($response) {
